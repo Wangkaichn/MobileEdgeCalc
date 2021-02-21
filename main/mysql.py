adbbc08688a87488d2cd8bc00501882241d34b5b
wangkai <1137120948@qq.com>
2021-02-21 09:38:11 +0800

    f

--- main/mysql.py
+++ main/mysql.py
@@ -15,10 +15,10 @@ def check_it():
   # 使用 cursor() 方法创建一个 dict 格式的游标对象 cursor
   cursor = conn.cursor(pymysql.cursors.DictCursor)
   # 使用 execute()  方法执行 SQL 查询
-  cursor.execute("select * from user")
+  cursor.execute("select * from test")
   # 使用 fetchone() 方法获取单条数据.
   data = cursor.fetchone()
-  print("-- 当前数量: ", data)
+  print("-- 当前数据: ", data)
   # 关闭数据库连接
   cursor.close()
   conn.close()
