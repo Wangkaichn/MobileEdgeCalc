2c7900a654643a71426e55bdcba302757dc35807
wangkai <1137120948@qq.com>
2021-02-21 09:23:27 +0800

    f

--- main/mysql.py
+++ main/mysql.py
@@ -1,1 +1,28 @@
+import pymysql
 
+HOST = 'localhost'
+PORT = 3306
+DATABASE = 'ylwawj'
+USER = 'root'
+PASSWORD = 'Wo123456'
+
+def get_connection():
+  conn = pymysql.connect(host=host, port=port, db=db, user=user, password=password)
+  return conn
+
+def check_it():
+  conn = get_connection()
+  # 使用 cursor() 方法创建一个 dict 格式的游标对象 cursor
+  cursor = conn.cursor(pymysql.cursors.DictCursor)
+  # 使用 execute()  方法执行 SQL 查询
+  cursor.execute("select * from user")
+  # 使用 fetchone() 方法获取单条数据.
+  data = cursor.fetchone()
+  print("-- 当前数量: ", data)
+  # 关闭数据库连接
+  cursor.close()
+  conn.close()
+
+
+if __name__ == '__main__':
+    check_it()
